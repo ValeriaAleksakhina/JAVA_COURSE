f4772ef5457999cc2200fafa321c26c2f525101a
Алексахина Валерия Владимировна <v.aleksakhina@bft.ru>
2025-03-25 13:12:58 +0300

    add lesson 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,30 +13,69 @@ public class Main {
 
        // Создаем несколько экземпляров класса Car
 
-        Car bmw = new Car("BMW", 2025);
-        Car toyota = new Car("Toyota", 2015);
-        Car lada = new Car("lada", 1997);
-
-        // создаем экземпляр с другим конструктором
-        Car volvo = new Car(2020);
-
-        // вызываем метод для всех экземпляров
-        bmw.makeBeBe();
-        lada.makeBeBe();
-        volvo.makeBeBe();
-        toyota.makeBeBe();
-
-        // переопределяем переменные
-        toyota.setYear(2020);
-        lada.setYear(2000);
-
-        // вызываем метод, чтобы убедиться, что изменения применились
-        toyota.makeBeBe();
-        lada.makeBeBe();
-
-
+//        Car bmw = new Car("BMW", 2025);
+//        Car toyota = new Car("Toyota", 2015);
+//        Car lada = new Car("lada", 1997);
+//
+//        // создаем экземпляр с другим конструктором
+//        Car volvo = new Car(2020);
+//
+//        // вызываем метод для всех экземпляров
+//        bmw.makeBeBe();
+//        lada.makeBeBe();
+//        volvo.makeBeBe();
+//        toyota.makeBeBe();
+//
+//        // переопределяем переменные
+//        toyota.setYear(2020);
+//        lada.setYear(2000);
+//
+//        // вызываем метод, чтобы убедиться, что изменения применились
+//        toyota.makeBeBe();
+//        lada.makeBeBe();
 
 
+        Scanner scanner = new Scanner(System.in);
+        final float rate = 0.07f;
+        float deposit = 0;
+        int month = 0;
+
+        while (true) {
+            try {
+                System.out.println("Введите сумму вклада: ");
+                deposit = scanner.nextFloat();
+                if (deposit <= 0) {
+                    System.out.println("Сумма вклада должна быть больше нуля!");
+                    continue;
+                } break;
+            } catch (InputMismatchException e){
+                System.out.println("Введите число!");
+                scanner.next();
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.println("Введите количество месяцев: ");
+                month = scanner.nextInt();
+                if (month <= 0) {
+                    System.out.println("Количество меясцев должно быть больше нуля!");
+                    continue;
+                } break;
+            } catch (InputMismatchException e) {
+                System.out.println("Введите целое число");
+                scanner.next();
+            }
+        }
+
+
+        for (int i = 0; i < month; i++) {
+            deposit += deposit * rate;
+        }
+
+        System.out.println("По итогам " + month + " месяцев общая сумма вклада стала " + deposit);
+
+        scanner.close();
 
 
     }
